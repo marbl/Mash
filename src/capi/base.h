0593b1b93e650d58b8adbd75b322c5190113fd8a
Jordi Montes <jomsdev@gmail.com>
2017-03-03 19:50:06 -0800

    Skeleton for mash capi created

---
+++ src/capi/base.h
@@ -0,0 +1,10 @@
+#ifndef BASE_C_H
+#define BASE_C_H
+
+// Some tools require special API declaration. Customizing the
+// MASH_EXTERN_API allows this. The default is simply nothing.
+#ifndef MASH_EXTERN_API
+#define MASH_EXTERN_API
+#endif
+
+#endif /* ifndef BASE_C_H */
