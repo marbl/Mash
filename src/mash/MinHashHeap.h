604737feb27dd4f04621a8167c981baf9505de50
Brian Ondov <brian.ondov@nih.gov>
2017-06-26 18:50:12 -0400

    handle input with no valid k-mers

--- src/mash/MinHashHeap.h
+++ src/mash/MinHashHeap.h
@@ -42,8 +42,8 @@ private:
     uint64_t kmersUsed;
 };
 
-inline double MinHashHeap::estimateMultiplicity() const {return (double)multiplicitySum / hashes.size();}
-inline double MinHashHeap::estimateSetSize() const {return pow(2.0, use64 ? 64.0 : 32.0) * (double)hashes.size() / (use64 ? (double)hashesQueue.top().hash64 : (double)hashesQueue.top().hash32);}
+inline double MinHashHeap::estimateMultiplicity() const {return hashes.size() ? (double)multiplicitySum / hashes.size() : 0;}
+inline double MinHashHeap::estimateSetSize() const {return hashes.size() ? pow(2.0, use64 ? 64.0 : 32.0) * (double)hashes.size() / (use64 ? (double)hashesQueue.top().hash64 : (double)hashesQueue.top().hash32) : 0;}
 inline void MinHashHeap::toHashList(HashList & hashList) const {hashes.toHashList(hashList);}
 inline void MinHashHeap::toCounts(std::vector<uint32_t> & counts) const {hashes.toCounts(counts);}
 
