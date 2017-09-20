24873e343a6d3d812a948003f4df95da3fd0e97d
Brian Ondov <brian.ondov@nih.gov>
2017-09-20 10:59:30 -0400

    screen: help fix

--- src/mash/CommandScreen.cpp
+++ src/mash/CommandScreen.cpp
@@ -40,7 +40,7 @@ CommandScreen::CommandScreen()
 {
 	name = "screen";
 	summary = "Determine whether query sequences are within a larger pool of sequences.";
-	description = "Determine how well query sequences are contained within a pool of sequences. The queries must be formatted as a single Mash sketch file (.msh), created with the `mash sketch` command. The <pool> files can be contigs or reads, in fasta or fastq, gzipped or not, and \"-\" can be given for <pool> to read from standard input. The <pool> sequences are assumed to be nucleotides, The output fields are [distance, shared-hashes, median-multiplicity, p-value, query-ID, query-comment], where median-multiplicity is computed for shared hashes, based on the number of observations of those hashes within the pool.";
+	description = "Determine how well query sequences are contained within a pool of sequences. The queries must be formatted as a single Mash sketch file (.msh), created with the `mash sketch` command. The <pool> files can be contigs or reads, in fasta or fastq, gzipped or not, and \"-\" can be given for <pool> to read from standard input. The <pool> sequences are assumed to be nucleotides, and will be 6-frame translated if the <queries> are amino acids. The output fields are [identity, shared-hashes, median-multiplicity, p-value, query-ID, query-comment], where median-multiplicity is computed for shared hashes, based on the number of observations of those hashes within the pool.";
     argumentString = "<queries>.msh <pool> [<pool>] ...";
 	
 	useOption("help");
