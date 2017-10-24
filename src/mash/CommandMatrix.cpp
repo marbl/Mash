87c129e6d3acdd25b9ed9765b63b15e94460a49e
Fabian Klötzl <fabian@kloetzl.info>
2017-10-24 15:32:16 +0200

    matrix: strip things from header which do not belong there

--- src/mash/CommandMatrix.cpp
+++ src/mash/CommandMatrix.cpp
@@ -25,6 +25,72 @@ namespace mash {
 
 std::string extract_name(const std::string &file_name);
 
+struct CompareInput
+{
+    CompareInput(const Sketch & sketchRefNew, const Sketch & sketchQueryNew, uint64_t indexRefNew, uint64_t indexQueryNew, uint64_t pairCountNew, const Sketch::Parameters & parametersNew, double maxPValueNew)
+        :
+        sketchRef(sketchRefNew),
+        sketchQuery(sketchQueryNew),
+        indexRef(indexRefNew),
+        indexQuery(indexQueryNew),
+        pairCount(pairCountNew),
+        parameters(parametersNew),
+        maxPValue(maxPValueNew)
+        {}
+    
+    const Sketch & sketchRef;
+    const Sketch & sketchQuery;
+    
+    uint64_t indexRef;
+    uint64_t indexQuery;
+    uint64_t pairCount;
+    
+    const Sketch::Parameters & parameters;
+    double maxPValue;
+};
+
+struct CompareOutput
+{
+    CompareOutput(const Sketch & sketchRefNew, const Sketch & sketchQueryNew, uint64_t indexRefNew, uint64_t indexQueryNew, uint64_t pairCountNew)
+        :
+        sketchRef(sketchRefNew),
+        sketchQuery(sketchQueryNew),
+        indexRef(indexRefNew),
+        indexQuery(indexQueryNew),
+        pairCount(pairCountNew)
+    {
+        pairs = new PairOutput[pairCount];
+    }
+    
+    ~CompareOutput()
+    {
+        delete [] pairs;
+    }
+    
+    struct PairOutput
+    {
+        uint64_t numer;
+        uint64_t denom;
+        double distance;
+        double pValue;
+        bool pass;
+    };
+    
+    const Sketch & sketchRef;
+    const Sketch & sketchQuery;
+    
+    uint64_t indexRef;
+    uint64_t indexQuery;
+    uint64_t pairCount;
+    
+    PairOutput * pairs;
+};
+
+void writeOutput(CompareOutput * output, bool table);
+CompareOutput * compare(CompareInput * input);
+void compareSketches(CompareOutput::PairOutput * output, const Sketch::Reference & refRef, const Sketch::Reference & refQry, uint64_t sketchSize, int kmerSize, double kmerSpace, double maxPValue);
+double pValue(uint64_t x, uint64_t lengthRef, uint64_t lengthQuery, double kmerSpace, uint64_t sketchSize);
+
 CommandMatrix::CommandMatrix()
 : Command()
 {
@@ -161,12 +227,12 @@ int CommandMatrix::run() const
     return 0;
 }
 
-CommandMatrix::CompareOutput * compare(CommandMatrix::CompareInput * input)
+CompareOutput * compare(CompareInput * input)
 {
     const Sketch & sketchRef = input->sketchRef;
     const Sketch & sketchQuery = input->sketchQuery;
     
-    CommandMatrix::CompareOutput * output = new CommandMatrix::CompareOutput(input->sketchRef, input->sketchQuery, input->indexRef, input->indexQuery, input->pairCount);
+    CompareOutput * output = new CompareOutput(input->sketchRef, input->sketchQuery, input->indexRef, input->indexQuery, input->pairCount);
     
     uint64_t sketchSize = sketchQuery.getMinHashesPerWindow() < sketchRef.getMinHashesPerWindow() ?
         sketchQuery.getMinHashesPerWindow() :
@@ -191,7 +257,7 @@ CommandMatrix::CompareOutput * compare(CommandMatrix::CompareInput * input)
     return output;
 }
 
-void compareSketches(CommandMatrix::CompareOutput::PairOutput * output, const Sketch::Reference & refRef, const Sketch::Reference & refQry, uint64_t sketchSize, int kmerSize, double kmerSpace, double maxPValue)
+void compareSketches(CompareOutput::PairOutput * output, const Sketch::Reference & refRef, const Sketch::Reference & refQry, uint64_t sketchSize, int kmerSize, double kmerSpace, double maxPValue)
 {
     uint64_t i = 0;
     uint64_t j = 0;
@@ -200,7 +266,6 @@ void compareSketches(CommandMatrix::CompareOutput::PairOutput * output, const Sk
     const HashList & hashesSortedRef = refRef.hashesSorted;
     const HashList & hashesSortedQry = refQry.hashesSorted;
     
-    output->pass = false;
     
     while ( denom < sketchSize && i < hashesSortedRef.size() && j < hashesSortedQry.size() )
     {
@@ -264,12 +329,7 @@ void compareSketches(CommandMatrix::CompareOutput::PairOutput * output, const Sk
     output->distance = distance;
     output->pValue = pValue(common, refRef.length, refQry.length, kmerSpace, denom);
     
-    if ( output->pValue > maxPValue )
-    {
-        return;
-    }
-    
-    output->pass = true;
+    output->pass = output->pValue <= maxPValue;
 }
 
 std::string extract_name(const std::string &file_name)
--- src/mash/CommandMatrix.h
+++ src/mash/CommandMatrix.h
@@ -1,6 +1,6 @@
-// Copyright © 2015, Battelle National Biodefense Institute (BNBI);
+// Copyright © 2015,2017 Battelle National Biodefense Institute (BNBI);
 // all rights reserved. Authored by: Brian Ondov, Todd Treangen,
-// Sergey Koren, and Adam Phillippy
+// Sergey Koren, Adam Phillippy, and Fabian Klötzl
 //
 // See the LICENSE.txt file included with this software for license information.
 
@@ -15,80 +15,10 @@ namespace mash {
 class CommandMatrix : public Command
 {
 public:
-    
-    struct CompareInput
-    {
-        CompareInput(const Sketch & sketchRefNew, const Sketch & sketchQueryNew, uint64_t indexRefNew, uint64_t indexQueryNew, uint64_t pairCountNew, const Sketch::Parameters & parametersNew, double maxPValueNew)
-            :
-            sketchRef(sketchRefNew),
-            sketchQuery(sketchQueryNew),
-            indexRef(indexRefNew),
-            indexQuery(indexQueryNew),
-            pairCount(pairCountNew),
-            parameters(parametersNew),
-            maxPValue(maxPValueNew)
-            {}
-        
-        const Sketch & sketchRef;
-        const Sketch & sketchQuery;
-        
-        uint64_t indexRef;
-        uint64_t indexQuery;
-        uint64_t pairCount;
-        
-        const Sketch::Parameters & parameters;
-        double maxPValue;
-    };
-    
-    struct CompareOutput
-    {
-        CompareOutput(const Sketch & sketchRefNew, const Sketch & sketchQueryNew, uint64_t indexRefNew, uint64_t indexQueryNew, uint64_t pairCountNew)
-            :
-            sketchRef(sketchRefNew),
-            sketchQuery(sketchQueryNew),
-            indexRef(indexRefNew),
-            indexQuery(indexQueryNew),
-            pairCount(pairCountNew)
-        {
-            pairs = new PairOutput[pairCount];
-        }
-        
-        ~CompareOutput()
-        {
-            delete [] pairs;
-        }
-        
-        struct PairOutput
-        {
-            uint64_t numer;
-            uint64_t denom;
-            double distance;
-            double pValue;
-            bool pass;
-        };
-        
-        const Sketch & sketchRef;
-        const Sketch & sketchQuery;
-        
-        uint64_t indexRef;
-        uint64_t indexQuery;
-        uint64_t pairCount;
-        
-        PairOutput * pairs;
-    };
-    
     CommandMatrix();
     
-    int run() const; // override
-    
-private:
-    
-    void writeOutput(CompareOutput * output, bool table) const;
+    int run() const; // override    
 };
-
-CommandMatrix::CompareOutput * compare(CommandMatrix::CompareInput * input);
-void compareSketches(CommandMatrix::CompareOutput::PairOutput * output, const Sketch::Reference & refRef, const Sketch::Reference & refQry, uint64_t sketchSize, int kmerSize, double kmerSpace, double maxPValue);
-double pValue(uint64_t x, uint64_t lengthRef, uint64_t lengthQuery, double kmerSpace, uint64_t sketchSize);
 
 } // namespace mash
 
