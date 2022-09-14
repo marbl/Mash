aeca2e424b3980d9a61590ca13fdbc2fc38c3edf
olsonanl <olson@mcs.anl.gov>
2022-09-14 13:47:54 -0500

    Fix parameter to munmap

--- src/mash/Sketch.cpp
+++ src/mash/Sketch.cpp
@@ -311,7 +311,7 @@ uint64_t Sketch::initParametersFromCapnp(const char * file)
     {
     	setAlphabetFromString(parameters, alphabetNucleotide);
     }
-	munmap(fd, fileInfo.st_size);
+	munmap(data, fileInfo.st_size);
 	close(fd);
 	
 	try
